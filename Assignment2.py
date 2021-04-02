84ef648c01631f629031ef16f2fc929b6a8af155
Mwasambu <81552851+Mwasambu@users.noreply.github.com>
2021-04-02 07:56:13 +0300

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,20 @@
+FullNames=input("Enter your first and Last names : ")
+County=input("Enter your Country of Origin : ")
+Profession=input("Enter the nature of your current job :")
+Email=input("Enter your email addresss :")
+Phone=input("Enter your phone number : ")
+City =input("Enter your current city of residence : ")
+Address=input("Enter your Mailing address/ Physical address: ")
+
+print("Names: " + FullNames)
+print("County: " + County)
+print("Profession : " + Profession)
+print("Email: " + Email)
+print("Phone: " + Phone)
+print("City: " + City)
+print("Address: " + Address)
+
+
+
+
+
